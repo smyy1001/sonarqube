3575cff968f2bed23affa67c5c9aa50b54072956
root <sumeyye@sumeyye-Yoga-Slim>
2024-06-12 10:35:04 +0300

    testing

---
+++ test.py
@@ -0,0 +1,30 @@
+import os.path
+import sys
+import pytest
+
+sys.path.append(os.path.join(os.getcwd(), '.'))
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+
+def pytest_runtest_setup(item):
+    # called for running each test in 'current' directory
+    print("setting up", item)
+
+
+@pytest.fixture(scope='session')
+def global_fixture():
+    """
+    Fixtures are callables decorated with @fixture
+    """
+    print("\n(global_fixture)")
+
+
+@pytest.fixture(scope='session')
+def env_vars():
+    return {
+    }
+
+@pytest.fixture(scope='session')
+def mock_data():
+    return {
+    }
